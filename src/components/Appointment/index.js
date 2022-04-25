f059afc0618a3e5289dae8d6e3948450f06aba94
Culganosi <culgan_@hotmail.com>
2022-04-25 08:35:09 -0400

    Implement custom hook and Interviewer selector

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,15 +3,46 @@ import "components/Appointment/styles.scss"
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import Form from './Form';
+import useVisualMode from 'hooks/useVisualMode';
 
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+
+const interviewers = [
+  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
+];
 
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
 
   return (
     <article className="appointment">
       <Header time={props.time} />
-       {props.interview ?
-       <Show student={props.interview.student} interviewer={[props.interview.interviewer]} /> : <Empty /> }
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+      <Show
+      student={props.interview.student}
+      interviewer={props.interview.interviewer}
+  />
+)}
+      {mode === CREATE && (
+      <Form
+      interviewers={interviewers}
+      onSave={props.onSave}
+      onCancel={back}
+      />
+      )}
+
+       {/* {props.interview ?
+       <Show student={props.interview.student} interviewer={[props.interview.interviewer]} /> : <Empty /> } */}
       </article>
   )
 }
