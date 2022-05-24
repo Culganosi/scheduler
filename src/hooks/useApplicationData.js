2ec0bd8dea2c841bfd360024080de83ba1ef2bfd
Culganosi <culgan_@hotmail.com>
2022-05-24 15:36:48 -0400

    change urls to utilize proxy, implement application tests

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -15,9 +15,9 @@ const useApplicationData = () => {
 
   useEffect(() => {
     Promise.all([
-      axios.get('http://localhost:8001/api/days'),
-      axios.get('http://localhost:8001/api/appointments'),
-      axios.get('http://localhost:8001/api/interviewers')
+      axios.get('/api/days'),
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
     ])
       .then((all) => {
         dispatch({ type: SET_APPLICATION_DATA,
@@ -26,20 +26,20 @@ const useApplicationData = () => {
           interviewers: all[2].data,
         });
       })
-      .catch((err) => {
-        console.log("error", err);
-      });
+      // .catch((err) => {
+      //   console.log("error", err);
+      // });
   }, []);
 
   function bookInterview(id, interview) {
-    return axios.put(`http://localhost:8001/api/appointments/${id}`, {interview})
+    return axios.put(`/api/appointments/${id}`, {interview})
       .then(() => {
         dispatch({ type: SET_INTERVIEW, id, interview });
       });
   };
 
   const cancelInterview = (id) => {
-    return axios.delete(`http://localhost:8001/api/appointments/${id}`)
+    return axios.delete(`/api/appointments/${id}`)
       .then(() => {
         dispatch({ type: SET_INTERVIEW, id, interview: null });
       });
